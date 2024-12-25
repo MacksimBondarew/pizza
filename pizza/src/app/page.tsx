9e7fbf43a35e295566287e2da9e44c46c07f449f
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-12-25 22:06:30 +0100

    add: products cards and instaled prisma, created database with beginning schemas

--- pizza/src/app/page.tsx
+++ pizza/src/app/page.tsx
@@ -1,22 +1,141 @@
-import { Container, Filters, Title, TopBar } from "@/components/shared";
+import {
+    Container,
+    Filters,
+    ProductGroupList,
+    Title,
+    TopBar,
+} from "@/components/shared";
 
 export default function Home() {
-    return <>
-        <Container className="mt-10">
-            <Title text="Все пиццы" size="lg" className="font-extralight" />
-        </Container>
-        <TopBar />
-        <Container className="pt-10 pb-14">
-            <div className="flex gap-[60px]">
-                {/* Filter */}
-                <div className="w-[250px]">
-                    <Filters />
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extralight" />
+            </Container>
+            <TopBar />
+            <Container className="pt-10 pb-14">
+                <div className="flex gap-[60px]">
+                    {/* Filter */}
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
+                    {/* Products list */}
+                    <div className="flex-1 gap-8 flex flex-wrap">
+                        <div className="flex flex-col gap-16">
+                            <ProductGroupList
+                                categoryId={1}
+                                title="Пицца"
+                                items={[
+                                    {
+                                        id: 1,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 2,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 3,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 4,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 5,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 6,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },                                  {
+                                        id: 7,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                ]}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-16">
+                            <ProductGroupList
+                                categoryId={2}
+                                title="Комбо"
+                                items={[
+                                    {
+                                        id: 1,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 2,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 3,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 4,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 5,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                    {
+                                        id: 6,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },                                  {
+                                        id: 7,
+                                        name: "piza",
+                                        price: 321,
+                                        imageUrl: "https://media.dodostatic.com/image/r:292x292/11eee23f6e2e1634b8788fab140d56b7.avif",
+                                        ingredients: "piza pizza pizza",
+                                    },
+                                ]}
+                            />
+                        </div>
+                    </div>
                 </div>
-                {/* Products list */}
-                <div className="flex-1">
-                    <div className="flex flex-col gap-16">Список товаров</div>
-                </div>
-            </div>
-        </Container>
-    </>;
+            </Container>
+        </>
+    );
 }
--- pizza/src/components/shared/categories.tsx
+++ pizza/src/components/shared/categories.tsx
@@ -1,19 +1,20 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 import React from 'react';
 
 interface Props {
   className?: string;
 }
 
-const items = ["a", "b", "c", "d", "e", "f"];
-const activeIndex = 0;
+const items = ["Пицца", "Комбо", "Суши", "d", "e", "f"];
 
 export const Categories: React.FC<Props> = ({ className }) => {
+    const categoryActiveId = useCategoryStore((state) => state.activeId);
     return (<div className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}>
         {items.map((item, index) => (
-            <a href="dwq" className={cn('flex items-center font-bold h-11 rounded-2xl px-5', activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary')} key={index}>
+            <a href={`/#${item}`} className={cn('flex items-center font-bold h-11 rounded-2xl px-5', categoryActiveId === index + 1 && 'bg-white shadow-md shadow-gray-200 text-primary')} key={index}>
                 <button>{item}</button>
             </a>
         ))}
