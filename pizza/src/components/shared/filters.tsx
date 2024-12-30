fd99a5d86815b8874798b36168a0ae4863494b20
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-12-30 18:10:10 +0100

    add: RangeSlider

--- pizza/src/components/shared/filters.tsx
+++ pizza/src/components/shared/filters.tsx
@@ -10,8 +10,24 @@ interface Props {
     className?: string;
 }
 
+interface PriceProps {
+    priceFrom: number;
+    priceTo: number;
+}
+
 export const Filters: React.FC<Props> = ({ className }) => {
     const { ingredients, loading, onAddId, selectedIds } = useFilterIngridients();
+    const [price, setPrice] = React.useState<PriceProps>({
+        priceFrom: 0,
+        priceTo: 1000
+    })
+
+    const updatePrice = (name: keyof PriceProps, value: number) => {
+        setPrice({
+            ...price,
+            [name]: value
+        })
+    };
 
     const items = ingredients.map((item) => ({ 
         id: String(item.id), 
@@ -30,18 +46,22 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <div className="mt-5 border-y border-y-neutral-100 pt-6 pb-7">
                 <p className="font-bold mb-3">Цена от и до:</p>
                 <div className="flex gap-3 mb-5">
-                    <Input type="number" placeholder="0" min={0} max={1000} />
+                    <Input type="number" placeholder="0" value={String(price.priceFrom)} onChange={(e) => updatePrice("priceFrom", Number(e.target.value))} min={0} max={1000} />
                     <Input
                         type="number"
                         min={100}
+                        value={String(price.priceTo)}
                         max={1000}
                         placeholder="1000"
+                        onChange={(e) => updatePrice("priceTo", Number(e.target.value))}
                     />
                 </div>
                 <RangeSlider
                     min={0}
                     max={1000}
                     step={10}
+                    onValueChange={([from, to]) => setPrice({ priceFrom: from, priceTo: to })}
+                    value={[price.priceFrom, price.priceTo]}
                 />
             </div>
             <CheckboxFiltersGroup title="Ингредиенты:" className="mt-5" limit={6} defaultItems={items.slice(0, 6)} items={items} loading={loading} onClickCheckbox={onAddId} selected={selectedIds} />
